598ba7e38df1a214371e78257dd7f5ac7107808b
Haseeb <moh.dhaseeb720@gmail.com>
2020-11-12 12:04:39 +0530

    Header and footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,9 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes'
 import { Component } from 'react';
 import DishDetail from './DishdetailComponent';
-
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 class Main extends Component {
 
@@ -15,20 +15,17 @@ class Main extends Component {
         }
     }
     onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId});
-      }
-    
+        this.setState({ selectedDish: dishId });
+    }
+
 
     render() {
         return (
             <div>
-                <Navbar dark color="primary">
-                    <div className="container">
-                        <NavbarBrand href="/">Rista Confu</NavbarBrand>
-                    </div>
-                </Navbar>
+                <Header></Header>
                 <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail dish={ this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0] } />
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+                <Footer></Footer>
             </div>
         );
     }
