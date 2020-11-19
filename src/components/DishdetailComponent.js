2024d44e193726379a350f226b867ea7787d8004
Haseeb <moh.dhaseeb720@gmail.com>
2020-11-19 08:57:11 +0530

    Prettier

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,83 +1,86 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDish({ dish }) {
-    return (
-        <div className="col-12 col-md-5 m-1">
-            <Card>
-                <CardImg width='100%' src={dish.image} alt={dish.name}></CardImg>
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-            </Card>
-        </div>
-    );
+  return (
+    <div className="col-12 col-md-5 m-1">
+      <Card>
+        <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 
 function formatDate(date) {
-    const option = { year: 'numeric', month: 'short', day: 'numeric' };
-    const newdate = new Date(date).toLocaleDateString("en-US", option)
-    return newdate;
-
+  const option = { year: "numeric", month: "short", day: "numeric" };
+  const newdate = new Date(date).toLocaleDateString("en-US", option);
+  return newdate;
 }
 
-
 function RenderComments({ comments }) {
-
-    if (comments != null) {
-        return (
-            <div className="col-12 col-md-5 m-1">
-                <h4>Comments</h4>
-                <ul className="list-unstyled">
-                    {comments.map((comment) => {
-                        return (
-                            <li key={comment.id}>
-                                <p>{comment.comment}</p>
-                                <p>{`-- ${comment.author} , ${formatDate(comment.date)}`}</p>
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        )
-    }
-    else {
-        return (<div></div>);
-    }
+  if (comments != null) {
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <h4>Comments</h4>
+        <ul className="list-unstyled">
+          {comments.map((comment) => {
+            return (
+              <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>{`-- ${comment.author} , ${formatDate(comment.date)}`}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 }
 
 const DishDetail = (props) => {
-    if (props.dish != null) {
-        return (
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
+  if (props.dish != null) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
 
-                    <div className="col-12">
-                        <h3>{props.dish.name}</h3>
-                        <hr></hr>
-                    </div>
-                </div>
-                <div className="row">
-                    <RenderDish dish={props.dish}></RenderDish>
-                    <RenderComments comments={props.comments} />
-                </div>
-            </div>
-
-        )
-
-    }
-    else {
-        return (<div></div>);
-    }
-
-}
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr></hr>
+          </div>
+        </div>
+        <div className="row">
+          <RenderDish dish={props.dish}></RenderDish>
+          <RenderComments comments={props.comments} />
+        </div>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
+};
 
 export default DishDetail;
