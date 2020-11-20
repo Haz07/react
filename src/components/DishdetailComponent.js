a1bf401046f92f5539d7baf31944256f8a808acf
Haseeb <moh.dhaseeb720@gmail.com>
2020-11-20 09:21:31 +0530

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -34,7 +34,12 @@ class CommentForm extends Component {
 
   handleSubmit(values) {
     console.log("button  " + JSON.stringify(values));
-    alert("button  " + JSON.stringify(values));
+    this.props.addComment(
+      this.props.dishId,
+      values.rating,
+      values.author,
+      values.comment
+    );
   }
 
   toggleModal() {
@@ -88,6 +93,7 @@ class CommentForm extends Component {
                     placeholder="Your Name"
                     className="form-control"
                     validators={{
+                      required,
                       minLength: minLength(3),
                       maxLength: maxLength(15),
                     }}
@@ -150,7 +156,7 @@ function formatDate(date) {
   return newdate;
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
   if (comments != null) {
     return (
       <div className="col-12 col-md-5 m-1">
@@ -165,7 +171,7 @@ function RenderComments({ comments }) {
             );
           })}
         </ul>
-        <CommentForm></CommentForm>
+        <CommentForm dishId={dishId} addComment={addComment}></CommentForm>
       </div>
     );
   } else {
@@ -195,7 +201,11 @@ const DishDetail = (props) => {
         </div>
         <div className="row">
           <RenderDish dish={props.dish}></RenderDish>
-          <RenderComments comments={props.comments} />
+          <RenderComments
+            comments={props.comments}
+            addComment={props.addComment}
+            dishId={props.dish.id}
+          />
         </div>
       </div>
     );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import { connect } from "react-redux";
+import { addComment } from "../redux/ActionCreators";
 
 const mapStateToProps = (state) => {
   return {
@@ -18,6 +19,10 @@ const mapStateToProps = (state) => {
   };
 };
 
+const mapDispatchToProps = (dispatch) => ({
+  addComment: (dishId, rating, author, comment) =>
+    dispatch(addComment(dishId, rating, author, comment)),
+});
 class Main extends Component {
   constructor(props) {
     super(props);
@@ -47,6 +52,7 @@ class Main extends Component {
           comments={this.props.comments.filter(
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
+          addComment={this.props.addComment}
         ></DishDetail>
       );
     };
@@ -75,4 +81,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
