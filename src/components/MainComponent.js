c9847b0ef4f628d9ec60cc2a663d0da5c1fce0d2
Haseeb <moh.dhaseeb720@gmail.com>
2020-11-22 10:53:33 +0530

    React redux from revisitted

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import { connect } from "react-redux";
 import { addComment, fetchDishes } from "../redux/ActionCreators";
+import { actions } from "react-redux-form";
 
 const mapStateToProps = (state) => {
   return {
@@ -24,6 +25,9 @@ const mapDispatchToProps = (dispatch) => ({
     dispatch(addComment(dishId, rating, author, comment)),
   fetchDishes: () => {
     dispatch(fetchDishes());
+  },
+  resetFeedbackForm: () => {
+    dispatch(actions.reset("feedback"));
   },
 });
 
@@ -76,7 +80,15 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.props.dishes} />}
           ></Route>
-          <Route exact path="/contactus" component={Contact}></Route>
+          <Route
+            exact
+            path="/contactus"
+            component={() => (
+              <Contact
+                resetFeedbackForm={this.props.resetFeedbackForm}
+              ></Contact>
+            )}
+          ></Route>
           <Route path="/menu/:dishId" component={DishWithId}></Route>
           <Route
             exact
